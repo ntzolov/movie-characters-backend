e682b3836a7abd333577716314115b52c9515bc1
ntzolov <ntzolov@gmail.com>
2023-10-26 21:32:55 +0300

    config update

--- index.js
+++ index.js
@@ -5,10 +5,21 @@ const routes = require('./config/routes');
 
 const app = express();
 
-try {
-  serverConnect(app).then(() => databaseConnect());
-} catch (error) {
-  console.log(error);
-}
+// try {
+//   serverConnect(app).then(() => databaseConnect());
+// } catch (error) {
+//   console.log(error);
+// }
 
+const initializeServer = async (app) => {
+  try {
+    await databaseConnect();
+    serverConnect(app);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+initializeServer(app);
 routes(app);
