e3be3455c91f571791d3abf7f027f15f36ecf967
ntzolov <ntzolov@gmail.com>
2023-10-27 17:23:31 +0300

    update config

--- config/express.js
+++ config/express.js
@@ -4,9 +4,14 @@ const cors = require('cors');
 const port = process.env.PORT || 3030;
 
 module.exports = async (app) => {
-  app.use(express.urlencoded({ extended: false }));
-  app.use(express.json({ extended: false }));
-  app.use(cors());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json({ extended: true }));
+  app.use(
+    cors({
+      origin: 'https://movie-characters.onrender.com',
+    })
+  );
+  app.options('*', cors());
 
   app.listen(port, () => console.log(`Server is listening on port ${port}...`));
 };
--- controllers/charactersController.js
+++ controllers/charactersController.js
@@ -11,9 +11,9 @@ router.get('/', async (req, res) => {
       characters.push({ error: 'Not characters found!' });
     }
 
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    // res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Methods', 'GET, PUT, POST');
+    // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
     return res.status(200).json(characters);
   } catch (error) {
